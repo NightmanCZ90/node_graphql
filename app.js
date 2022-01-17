bb5e2cea5f5153b8d941a4fd2a13056c47e89905
NightmanCZ90 <novak.jan90@gmail.com>
2022-01-17 21:47:02 +0100

    feature: Be able to update post

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ mongoose.connect(MONGODB_URI)
   .catch(err => console.log(err));
 
 const clearImage = filePath => {
-  filePath = path.join(__dirname, '..', filePath);
+  filePath = path.join(__dirname, '.', filePath);
   fs.unlink(filePath, err => console.log(err));
 }
